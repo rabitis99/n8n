1495317befa2144691e7dcda54de257af3c15724
rabitis99 <kyungsu9914@gmail.com>
2025-12-04 22:00:07 +0900

    테스트

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -4,5 +4,5 @@ import ch.qos.logback.core.encoder.JsonEscapeUtil;
 
 public class Hello {
      System.out.println("This is a simple Java program.");
-
+    출처: https://whoishoo.tistory.com/86 [HOOAI:티스토리]
 }

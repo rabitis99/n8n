83dba1fae5ad83e7edcce20e70e8f3171e03fe6f
rabitis99 <kyungsu9914@gmail.com>
2025-12-04 21:27:31 +0900

    테스트

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -3,5 +3,5 @@ package com.example.demo;
 import ch.qos.logback.core.encoder.JsonEscapeUtil;
 
 public class Hello {
-     System.out.println("This is a simple Java program.")
+     System.out.println("This is a simple Java program.");
 }
